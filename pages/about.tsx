d57069cbf8c958db99a0f8c4f36215225bd012a6
Nurhudaaa <nurhuda.joantama14@gmail.com>
2022-02-25 19:37:37 +0700

    feat: add about pages

---
+++ pages/about.tsx
@@ -0,0 +1,25 @@
+import { Box, Container, Text } from "@chakra-ui/react";
+import React from "react";
+import MainLayout from "../components/layout/MainLayout";
+
+export default function About() {
+  return (
+    <MainLayout>
+      <Container maxW="container.md">
+        <Box py={2} borderBottom="4px" mb={7}>
+          <Text as="h1" fontSize="5xl" fontWeight="bold">
+            About
+          </Text>
+        </Box>
+        <Box maxW="container.md">
+          <Text as="p" fontSize="md" lineHeight="7" letterSpacing="wide">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo aspernatur porro doloremque. Earum culpa asperiores commodi vel deleniti minus vitae necessitatibus perferendis odit amet!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo aspernatur porro doloremque. Earum culpa asperiores commodi vel deleniti minus vitae necessitatibus perferendis odit amet!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo aspernatur porro doloremque. Earum culpa asperiores commodi vel deleniti minus vitae necessitatibus perferendis odit amet!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo aspernatur porro doloremque. Earum culpa asperiores commodi vel deleniti minus vitae necessitatibus perferendis odit amet!
+          </Text>
+        </Box>
+      </Container>
+    </MainLayout>
+  );
+}
