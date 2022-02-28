a34826ae4786c786c9f55b8618e793b37fc2e301
Nurhudaaa <nurhuda.joantama14@gmail.com>
2022-02-28 12:00:26 +0700

    refactor: refactoring blog index

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,49 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { SearchIcon } from "@chakra-ui/icons";
 import { getAllPosts, Post } from "../../lib/api";
-
-function Blog({ blog }: { blog: Post }) {
-  const tagBgColor = useColorModeValue("gray.200", "gray.700");
-  return (
-    <Link href={`/blog/${blog.slug}`} passHref>
-      <Box role="group" as="a">
-        <Box
-          _groupHover={{
-            outline: "2px solid",
-          }}
-          p={0.5}
-          rounded="lg"
-        >
-          <Box h={{ base: 250, sm: 370, md: 470, xl: 520 }} position="relative" rounded="lg" overflow="hidden">
-            <Image src={blog.thumbnail} alt={blog.title} layout="fill" objectFit="cover" />
-          </Box>
-        </Box>
-        <Box mx={1} mt={7} mb={5}>
-          <Text fontSize="md" color="gray.500">
-            {blog.date}
-          </Text>
-          <Text
-            fontSize={{ base: "lg", md: "xl" }}
-            fontWeight="bold"
-            _groupHover={{
-              textDecoration: "underline",
-            }}
-            mb={3}
-          >
-            {blog.title}
-          </Text>
-          <HStack spacing={2}>
-            {blog.tags.map((tag) => (
-              <Box key={tag} px={2} py={0.5} rounded="sm" bg={tagBgColor}>
-                <Text fontSize="sm">{tag}</Text>
-              </Box>
-            ))}
-          </HStack>
-        </Box>
-      </Box>
-    </Link>
-  );
-}
+import Blog from "../../components/blog/Blog";
 
 type BlogProps = {
   blogs: Post[];
@@ -56,10 +14,14 @@ type BlogProps = {
 export default function Index({ blogs }: BlogProps) {
   return (
     <MainLayout>
+      {/* Box on top */}
       <Box px={7} py={10} rounded="lg" bg={useColorModeValue("gray.100", "gray.700")}>
+        {/* Title */}
         <Text as="h1" fontSize="5xl" fontWeight="black" letterSpacing="widest">
           <Link href="/blog">Blog</Link>
         </Text>
+
+        {/* Search */}
         <InputGroup my={12} display="flex" alignItems="center">
           <InputLeftElement pointerEvents="none" top="unset" pl={2}>
             <SearchIcon />
@@ -67,6 +29,8 @@ export default function Index({ blogs }: BlogProps) {
           <Input placeholder="What you want to find ?" variant="outline" py={3} rounded="full" size="xl" w={450} />
         </InputGroup>
       </Box>
+
+      {/* Blog List */}
       <Box my={12}>
         <SimpleGrid columns={{ base: 2, md: 3 }} spacing={{ base: 3, md: 5, xl: 12 }}>
           {blogs.map((blog) => (
