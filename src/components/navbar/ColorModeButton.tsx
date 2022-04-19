6b8c30deecb6904ad437347f7f302de610fb4c60
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-04-19 10:00:58 +0700

    animation togle color button

--- src/components/navbar/ColorModeButton.tsx
+++ src/components/navbar/ColorModeButton.tsx
@@ -1,12 +1,24 @@
-import { Button, useColorMode } from "@chakra-ui/react";
 import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
-export default function ColorModeButton(props: any) {
+const ColorModeButton: React.FC = (props) => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Button onClick={toggleColorMode} {...props}>
-      {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-    </Button>
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <motion.div
+        style={{ display: "inline-block" }}
+        key={useColorModeValue("light", "dark")}
+        initial={{ scale: 0.2, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 1.5, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <IconButton onClick={toggleColorMode} aria-label="Toggle theme" icon={useColorModeValue(<MoonIcon />, <SunIcon />)}></IconButton>
+      </motion.div>
+    </AnimatePresence>
   );
-}
+};
+
+export default ColorModeButton;
