7e98bd353d522adeb0d35013a26ffbc2c8a4193a
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-04-19 13:43:54 +0700

    fix "button theme" : add props to button

--- src/components/navbar/ColorModeButton.tsx
+++ src/components/navbar/ColorModeButton.tsx
@@ -3,8 +3,10 @@ import { AnimatePresence, motion } from "framer-motion";
 import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
-const ColorModeButton: React.FC = (props) => {
-  const { colorMode, toggleColorMode } = useColorMode();
+import { ChakraProps } from "@chakra-ui/react";
+
+const ColorModeButton: React.FC<ChakraProps> = (props) => {
+  const { toggleColorMode } = useColorMode();
   return (
     <AnimatePresence exitBeforeEnter initial={false}>
       <motion.div
@@ -15,7 +17,7 @@ const ColorModeButton: React.FC = (props) => {
         exit={{ scale: 1.5, opacity: 0 }}
         transition={{ duration: 0.2 }}
       >
-        <IconButton onClick={toggleColorMode} aria-label="Toggle theme" icon={useColorModeValue(<MoonIcon />, <SunIcon />)}></IconButton>
+        <IconButton onClick={toggleColorMode} {...props} aria-label="Toggle theme" icon={useColorModeValue(<MoonIcon />, <SunIcon />)}></IconButton>
       </motion.div>
     </AnimatePresence>
   );
